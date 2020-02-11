a4f9550765d6e104a5bf52c397fda53e68e4b999
Andrew Buckley <andrewbuckley21@gmail.com>
2020-02-10 20:54:44 -0500

    Added command-line function calling
    See invoke_and_query.txt for details and instructions.

---
+++ invoke.js
@@ -0,0 +1,58 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const { Gateway, Wallets } = require('fabric-network');
+const path = require('path');
+
+const ccpPath = path.resolve(__dirname, '..', '..', 'first-network', 'connection-org1.json');
+
+async function main() {
+    try {
+        
+        // pack command line arguments into a fucntion execution list
+        var terminal = new Array(process.argv.length - 2);
+        for (let i = 2; i < process.argv.length; i++) {
+            terminal[i-2] = process.argv[i];
+        }
+
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const identity = await wallet.get('user1');
+        if (!identity) {
+            console.log('An identity for the user "user1" does not exist in the wallet');
+            console.log('Run the registerUser.js application before retrying');
+            return;
+        }
+
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccpPath, { wallet, identity: 'user1', discovery: { enabled: true, asLocalhost: true } });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
+
+        // Get the contract from the network.
+        const contract = network.getContract('fabcar');
+
+        // Submit the specified transaction.
+        await contract.submitTransaction(...terminal);
+        console.log('Transaction has been submitted');
+
+        // Disconnect from the gateway.
+        await gateway.disconnect();
+
+    } catch (error) {
+        console.error(`Failed to submit transaction: ${error}`);
+        process.exit(1);
+    }
+}
+
+
+main();
